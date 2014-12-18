530d25159e470f461cda030467e6b00734a98d1f
Alex Curran <aml.curran@gmail.com>
2014-12-18 17:22:12 +0000

    Revert "Revert 2112e9c..c6916c7"
    This reverts commit 0e6bc41064be361fb7a43e70c890111059578e55.

--- Cooking Timer/FirstViewController.m
+++ Cooking Timer/FirstViewController.m
@@ -7,10 +7,13 @@
 //
 
 #import "FirstViewController.h"
+#import "Cooking_Timer-Swift.h"
 
 @interface FirstViewController ()
 - (IBAction)buttonClicked:(id)sender;
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UITextField *timerName;
+@property (nonatomic, strong) NotificationController *notificationController;
 
 @end
 
@@ -19,6 +22,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.notificationController = [NotificationController new];
+    [self.notificationController checkNotificationSettings];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -28,9 +33,23 @@
 
 - (IBAction)buttonClicked:(id)sender {
     // Get the count down the user has entered into the date picker
+    NSTimeInterval countdown = [self.datePicker countDownDuration];
+    // Create a timer with the number of seconds, if we're in debug, make the countdown shorter
+#ifdef DEBUG
+    countdown = 15.0;
+#endif
     
-    // Create a timer with the number of seconds
-    
-    // Create an alert (or something) when the timer counts down
+    NotificationController *controller = [NotificationController new];
+    [controller send:countdown foodName:self.timerName.text];
 }
+
+- (void)timerPinged:(NSTimer *)timer
+{
+    // Pull the timer name out of dictionary
+    NSString *timerName = timer.userInfo[@"title"];
+    NSString *message = [NSString stringWithFormat:@"Your %@ is ready to take out the oven", timerName];
+    
+    [[[UIAlertView alloc] initWithTitle:@"Ping!" message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil] show];
+}
+
 @end
