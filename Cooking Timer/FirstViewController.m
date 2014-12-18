4b535f987bc637660d12fabd4714f54dd98bebd4
curraa01 <alexander.curran@bbc.co.uk>
2014-12-18 12:57:40 +0000

    Customised the message of the alert view to show what you're putting in oven

--- Cooking Timer/FirstViewController.m
+++ Cooking Timer/FirstViewController.m
@@ -11,6 +11,7 @@
 @interface FirstViewController ()
 - (IBAction)buttonClicked:(id)sender;
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UITextField *timerName;
 
 @end
 
@@ -30,15 +31,23 @@
     // Get the count down the user has entered into the date picker
     NSTimeInterval countdown = [self.datePicker countDownDuration];
     
-    // Create a timer with the number of seconds
-    [NSTimer scheduledTimerWithTimeInterval:countdown target:self selector:@selector(timerPinged:) userInfo:nil repeats:NO];
-
+    // Create a dictionary which holds the name of the picker
+    NSDictionary *dictionary = @{ @"title" : self.timerName.text };
     
+    // Create a timer with the number of seconds, if we're in debug, make the countdown shorter
+#ifdef DEBUG
+    countdown = 15.0;
+#endif
+    [NSTimer scheduledTimerWithTimeInterval:countdown target:self selector:@selector(timerPinged:) userInfo:dictionary repeats:NO];
 }
 
 - (void)timerPinged:(NSTimer *)timer
 {
-    [[[UIAlertView alloc] initWithTitle:@"Ping" message:nil delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil] show];
+    // Pull the timer name out of dictionary
+    NSString *timerName = timer.userInfo[@"title"];
+    NSString *message = [NSString stringWithFormat:@"Your %@ is ready to take out the oven", timerName];
+    
+    [[[UIAlertView alloc] initWithTitle:@"Ping!" message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil] show];
 }
 
 @end
